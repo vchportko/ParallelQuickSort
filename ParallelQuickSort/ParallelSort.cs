727fbdbb80143ab2fe84d86af1baace7ac151014
Veronika Shportko <Veronika_Shportko@epam.com>
2018-05-21 15:02:51 +0300

    Add project files.

---
+++ ParallelQuickSort/ParallelSort.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ParallelQuickSort
+{
+    public class ParallelkSort<T>
+    {
+        public static void ParallelQuickSort(T[] data, IComparer<T> comparer,
+            int maxDepth = 16, int minBlockSize = 2048)
+        {
+            // call the internal method
+            doSort(data, 0, data.Length - 1, comparer, 0, maxDepth, minBlockSize);
+        }
+        internal static void doSort(T[] data, int startIndex, int endIndex,
+            IComparer<T> comparer, int depth, int maxDepth, int minBlockSize)
+        {
+            if (startIndex < endIndex)
+            {
+                // if we have exceeded the depth threshold or there are
+                // fewer items than we would like, then use sequential sort
+                if (depth > maxDepth || endIndex - startIndex < minBlockSize)
+                {
+                    Quicksort(data, startIndex, endIndex - startIndex, comparer);
+                }
+                else
+                {
+                    // we need to parallelize
+                    int pivotIndex = partitionBlock(data, startIndex, endIndex, comparer);
+                    // recurse on the left and right blocks
+                    Task leftTask = Task.Factory.StartNew(() =>
+                    {
+                        doSort(data, startIndex, pivotIndex - 1, comparer,
+                            depth + 1, maxDepth, minBlockSize);
+                    });
+                    Task rightTask = Task.Factory.StartNew(() =>
+                    {
+                        doSort(data, pivotIndex + 1, endIndex, comparer,
+                            depth + 1, maxDepth, minBlockSize);
+                    });
+                    // wait for the tasks to complete
+                    Task.WaitAll(leftTask, rightTask);
+                }
+            }
+        }
+
+        private static void Quicksort(T[] arr, int left, int right, IComparer<T> comparer)
+        {
+            // If the list contains one or less element: no need to sort!
+            if (right <= left) return;
+
+            // Partitioning our list
+            var pivot = partitionBlock(arr, left, right, comparer);
+
+            // Sorting the left of the pivot
+            Quicksort(arr, left, pivot - 1, comparer);
+            // Sorting the right of the pivot
+            Quicksort(arr, pivot + 1, right, comparer);
+        }
+
+        internal static int partitionBlock(T[] data, int startIndex, int endIndex,
+            IComparer<T> comparer)
+        {
+            // get the pivot value - we will be comparing all
+            // of the other items against this value
+            T pivot = data[startIndex];
+            // put the pivot value at the end of block
+            swapValues(data, startIndex, endIndex);
+            // index used to store values smaller than the pivot
+            int storeIndex = startIndex;
+            // iterate through the items in the block
+            for (int i = startIndex; i < endIndex; i++)
+            {
+                // look for items that are smaller or equal to the pivot
+                if (comparer.Compare(data[i], pivot) <= 0)
+                {
+                    // move the value and increment the index
+                    swapValues(data, i, storeIndex);
+                    storeIndex++;
+                }
+            }
+            swapValues(data, storeIndex, endIndex);
+            return storeIndex;
+        }
+        internal static void swapValues(T[] data, int firstIndex, int secondIndex)
+
+        {
+            T holder = data[firstIndex];
+            data[firstIndex] = data[secondIndex];
+            data[secondIndex] = holder;
+        }
+    }
+}
---
+++ ParallelQuickSort/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ParallelQuickSort.Properties;
+
+namespace ParallelQuickSort
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Resources.Welcome);
+
+            Start:
+            Console.Write("\n" + Resources.InputNumberCount);
+            int countOfNumbers = Int32.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Count of processors\tTime for sort");
+
+            // generate some random source data
+            Random rnd = new Random();
+            int[] sourceData = new int[countOfNumbers];
+            for (int i = 0; i < sourceData.Length; i++)
+            {
+                sourceData[i] = rnd.Next(1, 100);
+            }
+
+            for (var countOfProcessors = 1; countOfProcessors <= 128; countOfProcessors *= 2)
+            {
+                int[] sourceDataToSort = new int[countOfNumbers];
+                for (int i = 0; i < countOfNumbers; i++)
+                {
+                    sourceDataToSort[i] = sourceData[i];
+                }
+
+                Console.WriteLine("\nInputed array:");
+                foreach (var element in sourceDataToSort)
+                {
+                    Console.Write(element + " ");
+                }
+                Console.WriteLine("\n");
+
+                Stopwatch t = new Stopwatch();
+                t.Start();
+
+                // perform the parallel sort
+                ParallelkSort<int>.ParallelQuickSort(sourceDataToSort, new IntComparer(), countOfProcessors);
+
+                t.Stop();
+                var timeForSort = t.Elapsed;
+
+                Console.WriteLine("Sorted array:");
+                foreach (var element in sourceDataToSort)
+                {
+                    Console.Write(element + " ");
+                }
+                Console.WriteLine("\n");
+
+                Console.WriteLine("Count of processors\t"+countOfProcessors + "\tTime for sort\t" + timeForSort.TotalMilliseconds + Resources.MilliSeconds+"\n--------------------");
+            }
+
+            goto Start;
+        }
+
+        public class IntComparer : IComparer<int>
+        {
+            public int Compare(int first, int second)
+            {
+                return first.CompareTo(second);
+            }
+        }
+    }
+}
+
